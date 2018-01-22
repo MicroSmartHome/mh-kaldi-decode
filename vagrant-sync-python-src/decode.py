e3fcab6140db658006cc4dc78a3434e2503ad4ea
Marc Püls <mpuels@gmail.com>
2018-01-22 18:21:09 +0100

    chore: Move Python scripts to sync folder

---
+++ vagrant-sync-python-src/decode.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from kaldiasr.nnet3 import KaldiNNet3OnlineModel, KaldiNNet3OnlineDecoder
+
+MODELDIR    = '/home/mpuels/mh-kaldi-models/kaldi-chain-voxforge-de-r20180119'
+MODEL       = 'nnet_tdnn_sp'
+WAVFILE     = '/home/mpuels/projects/py-kaldi-asr/data/single.wav'
+
+model   = KaldiNNet3OnlineModel   (MODELDIR, MODEL)
+decoder = KaldiNNet3OnlineDecoder (model)
+
+if decoder.decode_wav_file(WAVFILE):
+
+    print('%s decoding worked!' % model)
+
+    s, l = decoder.get_decoded_string()
+    print()
+    print("*****************************************************************")
+    print("**", s)
+    print("** %s likelihood:" % model, l)
+    print("*****************************************************************")
+    print()
+
+else:
+    print('%s decoding did not work :(' % model)
